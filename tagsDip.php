edeeff5b8c945461d6e1804f50e07ebff2558fa0
Saqib Razzaq <saqibrzzaq@gmail.com>
2019-04-28 09:55:59 +0500

    Replace "|" with "/"

--- tagsDip.php
+++ tagsDip.php
@@ -7,15 +7,15 @@ define('TAGS_DIP_PATH', __DIR__);
 define('TAGS_DIP_NAME', basename(TAGS_DIP_PATH));
 define('TAGS_DIP_URL', $addons->url(TAGS_DIP_NAME));
 
-$pages = TAGS_DIP_NAME . '|pages';
+$pages = TAGS_DIP_NAME . '/pages';
 $menu = array(
   'tags_dip' => array(
     'display_name' => 'TagsDip',
     'sub' => array(
-      'Custom CSS' => $pages . '|dip.php&action=css',
-      'Custom JS' => $pages . '|dip.php&action=js',
-      'Custom Header' => $pages . '|dip.php&action=header',
-      'Custom Footer' => $pages . '|dip.php&action=footer'
+      'Custom CSS' => $pages . '/dip.php&action=css',
+      'Custom JS' => $pages . '/dip.php&action=js',
+      'Custom Header' => $pages . '/dip.php&action=header',
+      'Custom Footer' => $pages . '/dip.php&action=footer'
     )
   )
 );
